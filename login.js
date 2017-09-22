4fd99fb3502e22198954fe68f31ae5e4d33b1945
Austin Irvine <aa1irvine@gmail.com>
2017-09-22 15:28:05 -0500

    Fix: Changed parameters // Popup Works

--- login.js
+++ login.js
@@ -6,9 +6,25 @@ Brief  This file provides a two-step verification password
        for logging into this profile on the website.
 */
 
-var password = "";
+var myName = "";
+var myPassword = "";
 
 function login()
 {
-	
+	//
+	alert("Program Running");
+}
+
+function Submit(name, password)
+{
+	myName = name;
+	myPassword = password;
+
+	Verify();
+}
+
+function Verify()
+{
+	//run something in the html
+	alert("Successsssss");
 }
