f56f00d49f0f92604305600e9668417aed14b209
Austin Irvine <aa1irvine@gmail.com>
2017-09-22 14:41:11 -0500

    Added base files for website.

---
+++ login.js
@@ -0,0 +1,14 @@
+/*
+Author Austin Irvine
+Date   Sept. 22, 2017
+File   login.js
+Brief  This file provides a two-step verification password
+       for logging into this profile on the website.
+*/
+
+var password = "";
+
+function login()
+{
+	
+}
